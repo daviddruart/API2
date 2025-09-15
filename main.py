2dc83c614b42f95c890789cf069fc3df5d527988
daviddruart <159968250+daviddruart@users.noreply.github.com>
2025-09-15 19:16:02 +0000

    correciones y creación de db users

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # configuración SQLite dentro del codespace
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-app.config["SQLALCHEMY_DATABASE _URI"] = "sqlite///" + os.path.join(BASE_DIR, "users.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(BASE_DIR, "users.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
